e900ec8b9bdb466200137c54a7e4dd0f0606d520
Davi J <davijosantos.dj@gmail.com>
2023-08-04 13:27:04 -0300

    Adição do dicionário de tokens

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     dados = FileStream('input.txt')
     lexer = CompilerLexer(dados)
 
+    # Manipulação do arquivo de tokens para acessar o ID de cada TOKEN pelo nome
     with open('gen/Compiler.tokens', 'r+') as tT:
         Tokens = tT.readlines()
     typeTokens = {}
@@ -17,14 +18,15 @@ if __name__ == '__main__':
         while i[j].isdigit():
             key.insert(0, i[j])
             j-=1
-        typeTokens[''.join(key)] = i[0:len(i)+j]
+        typeTokens[''.join(key)] = i[0:len(i)+j] # Dicionário de tokens
 
     output = ""
-    for tok in lexer.getAllTokens():    # Ajeitando a string para saída adequada no arquivo
+    for tok in lexer.getAllTokens():    # Manipulando a string para saída adequada no arquivo
         output += '<'+ typeTokens[str(tok.type)] + ',' + str(tok.text) +'>'+ '\n'
     lexer.reset()
     stream = CommonTokenStream(lexer)
     parser = CompilerParser(stream)
     tree = parser.prog()
+    #print(tree.toStringTree())
     with open('output.txt', 'w+') as saida:     # Imprimindo os tokens e os tipos dos tokens no arquivo
         saida.write(output)
